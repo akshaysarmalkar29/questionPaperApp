e1d7cf6f9fd317d8e5de7afc4346043fd04e4726
Akshay Sarmalkar <akshaysarmalkar29@gmail.com>
2021-03-12 11:18:14 +0530

    Add Auth Routes

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,52 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
 });
+
+router.get("/register", (req, res, next) => {
+  res.send("GET /register");
+})
+
+router.post("/register", (req, res, next) => {
+  res.send("POST /register");
+})
+
+router.get("/login", (req, res, next) => {
+  res.send("GET /login");
+})
+
+router.post("/login", (req, res, next) => {
+  res.send("POST /login");
+})
+
+router.get("/logout", (req, res, next) => {
+  res.send("GET /logout");
+})
+
+router.get("/profile/:user_id", (req, res, next) => {
+  res.send("GET /profile/:user_id");
+})
+
+router.put("/profile/:user_id", (req, res, next) => {
+  res.send("PUT /profile/:user_id");
+})
+
+router.get("/forgot", (req, res, next) => {
+  res.send("GET /forgot");
+})
+
+router.post("/forgot", (req, res, next) => {
+  res.send("POST /forgot");
+})
+
+router.get("/reset/:token", (req, res, next) => {
+  res.send("GET /reset/:token");
+})
+
+router.put("/reset/:token", (req, res, next) => {
+  res.send("PUT /reset/:token");
+})
 
 module.exports = router;
